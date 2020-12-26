36bda44ef5c220a7d71b03e1b491f6590e5b1001
Denis Leonov <denl@uber.com>
2020-12-26 17:43:03 +0200

    Improve build configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 /* eslint-env node */
 const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
 	entry: './src/index.js',
 	plugins: [new CleanWebpackPlugin()],
+	target: 'node',
+	externals: [nodeExternals()],
 	output: {
 		libraryTarget: 'umd',
 		library: 'aboutnumber',
@@ -12,6 +15,5 @@ module.exports = {
 		path: path.resolve(__dirname, './dist'),
 		filename: 'aboutnumber.js',
 		globalObject: 'this',
-		umdNamedDefine: true,
 	},
 };
